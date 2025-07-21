d06a2990274687e12a59cbcae545d8d0cc25168e
NichFos <nicholasdfoster645@gmail.com>
2025-07-21 16:42:25 -0500

    Made Vito balerica admin, created team admin file

--- balerica-admin-vpc.tf
+++ balerica-admin-vpc.tf
@@ -1,5 +1,8 @@
-resource "google_compute_network" "balerica_inc_main" {
-  name                            = "balerica-inc-main"
+# Vito Balerica Admin
+
+
+resource "google_compute_network" "vito_balerica_inc_main" {
+  name                            = "vito-balerica-inc-main"
   routing_mode                    = "REGIONAL"
   auto_create_subnetworks         = false
   mtu                             = 1460
@@ -8,19 +11,19 @@ resource "google_compute_network" "balerica_inc_main" {
 }
 
 
-resource "google_compute_subnetwork" "balerica_inc_private" {
+resource "google_compute_subnetwork" "vito_balerica_inc_private" {
   name                     = "balerica-inc-private"
-  ip_cidr_range            = "10.80.20.0/24"
+  ip_cidr_range            = "10.40.20.0/24"
   region                   = "southamerica-east1"
-  network                  = google_compute_network.balerica_inc_main.id 
+  network                  = google_compute_network.vito_balerica_inc_main.id 
   private_ip_google_access = true
   provider                 = google.balerica
 }
 
 
-resource "google_compute_firewall" "allow-ssh" {
-  name     = "allow-ssh"
-  network  = google_compute_network.balerica_inc_main.name 
+resource "google_compute_firewall" "hq-allow-ssh" {
+  name     = "hq-allow-ssh"
+  network  = google_compute_network.vito_balerica_inc_main.name 
   provider = google.balerica
 
   allow {
@@ -33,9 +36,9 @@ resource "google_compute_firewall" "allow-ssh" {
 
 
 
-resource "google_compute_firewall" "allow-icmp" {
-  name     = "allow-icmp"
-  network  = google_compute_network.balerica_inc_main.name 
+resource "google_compute_firewall" "hq-allow-icmp" {
+  name     = "hq-allow-icmp"
+  network  = google_compute_network.vito_balerica_inc_main.name  
   provider = google.balerica
 
   allow {
