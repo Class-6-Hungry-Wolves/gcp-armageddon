ce60bd2ab0cb97f60a2df28645f0023aebedbcfb
Jourdan Callahan <jourdancallahan8@gmail.com>
2025-07-21 18:30:47 -0400

    added vpc

--- hw-admin-vpc.tf
+++ hw-admin-vpc.tf
@@ -54,3 +54,32 @@ resource "google_network_connectivity_spoke" "team_spoke"  {
     uri = google_compute_network.hw_team_admin_main.self_link
   }
 }
+
+resource "google_compute_address" "team_vpn_static_ip" {
+  name = "team-vpn-static-ip"
+}
+
+variable "vpn_shared_secret" {}
+
+resource "google_compute_vpn_tunnel" "team-tunnel" {
+  name          = "team-tunnel"
+  peer_ip       = google_compute_address.balerciavpn_static_ip.address
+  shared_secret = var.vpn_shared_secret
+
+  target_vpn_gateway = google_compute_vpn_gateway.target_gateway.id
+
+  depends_on = [
+    google_compute_forwarding_rule.fr_esp,
+    google_compute_forwarding_rule.fr_udp500,
+    google_compute_forwarding_rule.fr_udp4500,
+  ]
+}
+
+resource "google_compute_route" "team-to-balerica-route" {
+  name       = "team-to-balerica-route"
+  network    = google_compute_network.vito_balerica_inc_main.name
+  dest_range = "10.80.20.0/24"
+  priority   = 1000
+
+  next_hop_vpn_tunnel = google_compute_vpn_tunnel.tunnel1.id
+}
