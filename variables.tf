15ac489709b82a304d1de169f3891425112deae8
NichFos <nicholasdfoster645@gmail.com>
2025-07-23 21:15:20 -0500

    Added variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "nick_vpn_shared_secret" {}
 variable "xavier_vpn_shared_secret" {}
 variable "jourdan_vpn_shared_secret" {}
 variable "joshua_vpn_shared_secret" {}
+
+
+variable "sa-roles" {
+  type = set(string)
+  default = ["roles/storage.admin", "roles/artifactregistry.admin", "roles/networkconnectivity.hubAdmin"]
+}
